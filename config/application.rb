ab570503650b5bbff83fd0b67d8fd6cf3bcdf0e6
yuki <yukisaki80@gmail.com>
2020-10-11 13:16:30 +0900

    RSpecテストの準備

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,11 @@ module TravelLog
 
     config.generators do |g|
       g.assets false          # CSS, JavaScriptファイルは生成しない
-      g.test_framework false  # testファイルは生成しない
+      g.test_framework :rspec,
+        controller_specs: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
     end
   end
 end
