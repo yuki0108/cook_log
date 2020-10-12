0a7f0cac2c8f8cae7b9cb65f6090576a6153c75a
yuki <yukisaki80@gmail.com>
2020-10-12 22:33:56 +0900

    ログイン・ログアウト機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root to: 'static_pages#home'
   get :about, to: 'static_pages#about'
   get :use_of_terms, to: 'static_pages#terms'
   get :signup,       to: 'users#new'
   resources :users
+  get    :login,     to: 'sessions#new'
+  post   :login,     to: 'sessions#create'
+  delete :logout,    to: 'sessions#destroy'
 end
