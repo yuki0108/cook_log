4c018d4ccc7bbc15900d3337d2a3d923a35ee6d4
yuki <yukisaki80@gmail.com>
2020-10-15 20:43:34 +0900

    ユーザー一覧実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,15 +2,16 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :show, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
 
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
 
   def show
     @user = User.find(params[:id])
-  end
-
-  def index
   end
 
   def create
