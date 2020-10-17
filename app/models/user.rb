c7bc190b6e982508091b28188aacd7a48c4385a4
yuki <yukisaki80@gmail.com>
2020-10-17 22:18:36 +0900

    料理登録ページ実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,6 +41,10 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
+  def feed
+    Dish.where("user_id = ?", id)
+  end
+
   private
     def downcase_email
       self.email = email.downcase
--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -7,6 +7,35 @@ RSpec.describe "StaticPages", type: :system do
         visit root_path
       end
 
+      it "正しいタイトルが表示されることを確認" do
+        expect(page).to have_title full_title
+      end
+
+      context "料理フィード", js: true do
+        let!(:user) { create(:user) }
+        let!(:dish) { create(:dish, user: user) }
+
+        before do
+          login_for_system(user)
+        end
+
+        it "料理のぺージネーションが表示されること" do
+          login_for_system(user)
+          create_list(:dish, 6, user: user)
+          visit root_path
+          expect(page).to have_content "みんなの料理 (#{user.dishes.count})"
+          expect(page).to have_css "div.pagination"
+          Dish.take(5).each do |d|
+            expect(page).to have_link d.name
+          end
+        end
+
+        it "「新しい料理を作る」リンクが表示されること" do
+          visit root_path
+          expect(page).to have_link "新しい料理を作る", href: new_dish_path
+        end
+      end
+
       it "トラベルログの文字列が存在することを確認" do
         expect(page).to have_content 'トラベルログ'
       end
