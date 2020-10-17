fa07c4f2baebb26e55643de788afcb16c6a6d150
yuki <yukisaki80@gmail.com>
2020-10-17 22:18:36 +0900

    料理登録ページ実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @dishes = @user.dishes.paginate(page: params[:page], per_page: 5)
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get :use_of_terms, to: 'static_pages#terms'
   get :signup,       to: 'users#new'
   resources :users
+  resources :dishes
   get    :login,     to: 'sessions#new'
   post   :login,     to: 'sessions#create'
   delete :logout,    to: 'sessions#destroy'
