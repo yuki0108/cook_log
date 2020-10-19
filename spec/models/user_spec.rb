cfe715d1df7d53e927cde8649081a78fb1714b6e
yuki <yukisaki80@gmail.com>
2020-10-19 19:37:32 +0900

    受動的なフォロー

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -66,6 +66,7 @@ RSpec.describe User, type: :model do
   context "フォロー機能" do
     it "フォローとアンフォローが正常に動作すること" do
       expect(user.following?(other_user)).to be_falsey
+      expect(other_user.followed_by?(user)).to be_truthy
       user.follow(other_user)
       expect(user.following?(other_user)).to be_truthy
       user.unfollow(other_user)
