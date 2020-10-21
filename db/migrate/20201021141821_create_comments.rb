a76f9c6bf4e24bc1a1201799d701b1897592b817
yuki <yukisaki80@gmail.com>
2020-10-21 23:24:52 +0900

    Commentモデルの作成

---
+++ db/migrate/20201021141821_create_comments.rb
@@ -0,0 +1,13 @@
+class CreateComments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :comments do |t|
+      t.integer :dish_id
+      t.integer :user_id
+      t.text :content
+
+      t.timestamps
+    end
+    add_index :comments, :user_id
+    add_index :comments, :dish_id
+  end
+end
