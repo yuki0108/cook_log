d2dff68e22b9ee557ca5d7d71b4790d311321fb1
yuki <yukisaki80@gmail.com>
2020-10-21 23:38:36 +0900

    コメントの登録

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,15 @@ class CommentsController < ApplicationController
   before_action :logged_in_user
 
   def create
+    @dish = Dish.find(params[:dish_id])
+    @user = @dish.user
+    @comment = @dish.comments.build(user_id: current_user.id, content: params[:comment][:content])
+    if !@dish.nil? && @comment.save
+      flash[:success] = "コメントを追加しました！"
+    else
+      flash[:danger] = "空のコメントは投稿できません。"
+    end
+    redirect_to request.referrer || root_url
   end
 
   def destroy
--- app/models/dish.rb
+++ app/models/dish.rb
@@ -17,6 +17,11 @@ class Dish < ApplicationRecord
             allow_nil: true
   validate  :picture_size
 
+  # 料理に付属するコメントのフィードを作成
+  def feed_comment(dish_id)
+    Comment.where("dish_id = ?", dish_id)
+  end
+
   private
 
     # アップロードされた画像のサイズを制限する
