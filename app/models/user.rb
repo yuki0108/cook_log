59945d1bee22cefcfaed3a5a6d61eab091d5a24b
yuki <yukisaki80@gmail.com>
2020-10-22 21:46:15 +0900

    Notificationモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :dishes, dependent: :destroy
+  has_many :notifications, dependent: :destroy
   has_many :active_relationships, class_name: "Relationship",
                                   foreign_key: "follower_id",
                                   dependent: :destroy
