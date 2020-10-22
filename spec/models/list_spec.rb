412a151ba25b9faa14b3102841de85d61ebc66b5
yuki <yukisaki80@gmail.com>
2020-10-22 22:54:18 +0900

    Listsコントローラ、ルーティングの作成

---
+++ spec/models/list_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe List, type: :model do
+  let!(:list) { create(:list) }
+
+  it "listインスタンスが有効であること" do
+    expect(list).to be_valid
+  end
+
+  it "user_idがnilの場合、無効であること" do
+    list.user_id = nil
+    expect(list).not_to be_valid
+  end
+
+  it "dish_idがnilの場合、無効であること" do
+    list.dish_id = nil
+    expect(list).not_to be_valid
+  end
+
+  it "from_user_idがnilの場合、無効であること" do
+    list.from_user_id = nil
+    expect(list).not_to be_valid
+  end
+end
