c3d02a5ae93f4b811ad22c8ac627ef3568be2b9f
yuki <yukisaki80@gmail.com>
2020-10-22 23:04:33 +0900

    リスト登録/削除ボタン

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,8 +5,22 @@ class ListsController < ApplicationController
   end
 
   def create
+    @dish = Dish.find(params[:dish_id])
+    @user = @dish.user
+    current_user.list(@dish)
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
+    end
   end
 
   def destroy
+    list = List.find(params[:list_id])
+    @dish = list.dish
+    list.destroy
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
+    end
   end
 end
