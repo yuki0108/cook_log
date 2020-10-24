3c7a7ca8b9feeb6fa782a64da92e5e823ebc8eb9
yuki <yukisaki80@gmail.com>
2020-10-24 13:01:44 +0900

    材料の更新

--- spec/requests/dishes_edit_spec.rb
+++ spec/requests/dishes_edit_spec.rb
@@ -38,7 +38,11 @@ RSpec.describe "料理編集", type: :request do
                                                tips: "ピリッと辛めに味付けするのがオススメ",
                                                reference: "https://cookpad.com/recipe/2798655",
                                                required_time: 30,
-                                               popularity: 5 } }
+                                               popularity: 5,
+                                               picture: picture2,
+                                               ingredients_attributes: [
+                                               name: "編集-じゃがいも",
+                                               quantity: "編集-2個"] } }
       expect(response).to have_http_status "302"
       expect(response).to redirect_to login_path
     end
