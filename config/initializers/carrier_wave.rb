4e8a50821595022abc8540050659265e170c00f6
yuki <yukisaki80@gmail.com>
2020-10-24 17:14:33 +0900

    Key変更

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,10 +3,10 @@ if Rails.env.production?
     config.fog_credentials = {
       # Amazon S3用の設定
       :provider              => 'AWS',
-      :region                => ENV['ap-northeast-1'],
-      :aws_access_key_id     => ENV['AKIASS2H7XBRMIMYVTP4'],
-      :aws_secret_access_key => ENV['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']
+      :region                => ENV['S3_REGION'],
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['cooklogapp']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
