99ba95bdfe6f9fb3721721e220cc2c04fcde7162
yuki <yukisaki80@gmail.com>
2020-10-24 13:24:42 +0900

    材料の表示

--- spec/system/dishes_spec.rb
+++ spec/system/dishes_spec.rb
@@ -162,6 +162,10 @@ RSpec.describe "Dishes", type: :system do
         expect(page).to have_content dish.reference
         expect(page).to have_content dish.required_time
         expect(page).to have_content dish.popularity
+        dish.ingredients.each do |i|
+          expect(page).to have_content i.name
+          expect(page).to have_content i.quantity
+        end
         expect(page).to have_link nil, href: dish_path(dish), class: 'dish-picture'
       end
     end
